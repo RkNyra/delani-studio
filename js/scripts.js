a7f53f2c0d8a49c30685056da8932594cc00c2d5
RKNyra <rk.nyra@gmail.com>
2019-08-25 14:23:22 +0300

    add toggle effect to the icons in the What we do section

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
-$(document).ready(function) {
+$(document).ready(function() {
+    $("#design, .design").click(function() {
+        $("#design").toggle();
+        $(".design").toggle();
+        document.querySelector("h4").style.fontWeight = "700";
+    });
 
-}
+    $("#development, .development").click(function() {
+        $("#development").toggle();
+        $(".development").toggle();
+        document.querySelector("h4").style.fontWeight = "700";
+    });
+
+    $("#management, .management").click(function() {
+        $("#management").toggle();
+        $(".management").toggle();
+        document.getElementsByTagName("h4").style.fontWeight = "700";
+    });
+});
